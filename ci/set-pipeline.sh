92fc3cd17b9260f073ab70c3f66f4da02f9cdf66
Huan Wang <huwang@pivotal.io>
2019-05-30 15:00:58 -0600

    update push-app pipeline to work on concourse cf denver [#166295939]
    Signed-off-by: Glenn Oppegard <goppegard@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-if ! lpass status --quiet; then
- printf "\n\e[1;31mRun 'lpass login YOUR_EMAIL' first\e[0m\n\n"
- exit
-fi
+lpass ls &> /dev/null
+
+fly -t cf-denver login -c https://concourse.cf-denver.com/ --team-name=main --open-browser
 
 current_dir=$(cd $(dirname $0) && pwd)
 config_file=${current_dir}/push-apps.yml
 
-fly --target superpipe set-pipeline --pipeline push-apps --config ${config_file} \
-    --load-vars-from <(lpass show --notes 'Shared-apm/concourse/healthwatch-product-secrets')
+fly --target cf-denver set-pipeline --pipeline push-apps --config ${config_file} \
+    --load-vars-from <(lpass show --notes 'Shared-CF-Denver-Pivotal/push-apps-ci')
